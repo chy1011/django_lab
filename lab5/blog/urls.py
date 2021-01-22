280401fe2505c59bd8a2ea7c9eedc55b0dd815e6
chy1011 <houyong.chan@gmail.com>
2021-01-22 19:29:00 +0800

    Lab6 completed - URL Patterns

--- lab5/blog/urls.py
+++ lab5/blog/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from website.views import welcome
+from posts.views import new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', welcome, name="home"),
-    path('posts/', include('posts.urls')),
+	path('', welcome),
+    path('new', new),
 ]
